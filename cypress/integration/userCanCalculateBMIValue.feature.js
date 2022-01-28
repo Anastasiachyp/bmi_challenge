fc510df6d0acacd8586dbfc66d64a6c42c67cf9b
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-01-28 12:10:48 +0100

    modify formula of BMI

--- cypress/integration/userCanCalculateBMIValue.feature.js
+++ cypress/integration/userCanCalculateBMIValue.feature.js
@@ -6,13 +6,13 @@ describe('user can calculate BMI value based on individual measure', () => {
                 cy.visit('http://localhost:3474') // ARRANGE
                 // ACT
                 cy.get('[data-cy=weight-input]').type('65')
-                cy.get('[data-cy=height-input]').type('1.70')
+                cy.get('[data-cy=height-input]').type('1.7')
                 cy.get('[data-cy=calculate-button]').click()
             });
     
                 it('is expected to display his BMI value', () => {
                     // ASSERT 
-                    cy.get('[data-cy=results]').should('contain.text', "Your BMI value is 22.5")
+                    cy.get('[data-cy=results]').should('contain.text', "Your BMI value is 22.5 and you are normal weight")
             });
         });    
         });
